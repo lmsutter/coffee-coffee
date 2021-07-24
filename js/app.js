399cffad2b42f7b0eb552450b732eca8b48d413e
John Doe <lucasmsutter@gmail.com>
2021-07-24 16:59:41 -0500

    finished home page and mobile
    Started working on coffee page scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
-let mobileButton = document.querySelector('.mobile-menu');
-let menu = document.querySelector('.home-header');
+let navExpander = document.querySelector('.nav-expander')
+let nav = document.querySelector('.nav') 
+let content = document.querySelector('.content')
+let pageWrapper = document.querySelector('.page-wrapper')
 
-mobileButton.addEventListener('click', () => {
-    menu.classList.toggle('popup-header');
-});
-
+navExpander.addEventListener('click', ()=> {
+    nav.classList.toggle('nav-open')
+    navExpander.classList.toggle('nav-expander-open')
+    content.classList.toggle('blur')
+    pageWrapper.classList.toggle('blur')
+})
